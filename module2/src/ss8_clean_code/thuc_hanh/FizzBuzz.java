97740734823f667fa6228539d9280bd2faa0d17c
hvh08112000 <hvh08112000@gmail.com>
2021-10-23 21:06:16 +0700

    bai tap ss8

---
+++ module2/src/ss8_clean_code/thuc_hanh/FizzBuzz.java
@@ -0,0 +1,19 @@
+package ss8_clean_code.thuc_hanh;
+
+public class FizzBuzz {
+    public static String fizzBuzz(int number){
+        boolean isFizz = number % 3 == 0;
+        boolean isBuzz = number % 5 == 0;
+        if(isFizz && isBuzz)
+            return "FizzBuzz";
+
+        if(number % 3 == 0)
+            return "Fizz";
+
+        if(number % 5 == 0)
+            return "Buzz";
+
+        return number + "";
+    }
+}
+
